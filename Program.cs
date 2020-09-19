b80256baeefbcd61514dceb2a943b00c8ac73cdd
inspiringhobbyist <inspiringhobbyist@gmail.com>
2020-09-19 16:02:04 -0600

    adding enum example

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace examples
+{
+    class Program
+    {
+        enum days { sun, mon, tue, wed, thurs,fri,sat}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"this is enum para {days.sun}");
+        }
+    }
+}
