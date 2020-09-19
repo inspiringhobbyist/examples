01ecc0ef4e2d5686b4f7331c023163277019109e
inspiringhobbyist <inspiringhobbyist@gmail.com>
2020-09-19 16:30:01 -0600

    adding array example

--- Program.cs
+++ Program.cs
@@ -4,11 +4,21 @@ namespace examples
 {
     class Program
     {
-        enum days { sun, mon, tue, wed, thurs,fri,sat}
+        enum days { sun=1, mon, tue, wed, thurs,fri,sat}
+
+       
         static void Main(string[] args)
         {
+            int[] values;
             Console.WriteLine("Hello World!");
             Console.WriteLine($"this is enum para {days.sun}");
+            values = new int[3];
+            values[0] = 1;
+            values[1] = 2;
+            values[2] = 3;
+            
+            Console.WriteLine($"{values[0]}");
+
         }
     }
 }
