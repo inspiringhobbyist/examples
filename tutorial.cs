88747c8afa10c046009009d827ac146c6acc43ac
inspiringhobbyist <inspiringhobbyist@gmail.com>
2020-09-19 19:19:05 -0600

    writing about types of access modifiers

--- tutorial.cs
+++ tutorial.cs
@@ -2,10 +2,14 @@ using System;
 namespace examples {
     class tutorial
     {
-         int tutorialid;
-         string tutorialname;
+         int tutorialid;    // property of class  tutorial
+         string tutorialname; //property of class tutorial
 
-         public void  setutorial(int pid, string name) {
+         public void  setutorial(int pid, string name) {     //method settutorial for class tutorial
+              /* three types of access modifiers for methods or property it could be either 
+                  public, accessible externally by any program or classs within same namespace
+                  private,   not accessible to any external program or class, other than current class 
+                  protected, is only accessible by those who inherit this class */ 
 
              tutorialid = pid;
              tutorialname = name;
